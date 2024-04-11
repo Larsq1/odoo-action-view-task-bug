ad26631285b6e6049862e0d80645f257614567d3
larsquaedvlieg <lars.quaedvlieg@codeforward.nl>
2024-04-11 08:12:40 +0200

    [ADD] do not use inheritance

--- xx_project/models/project_task.py
+++ xx_project/models/project_task.py
@@ -1,4 +1,5 @@
-from odoo import fields, api, models
+from odoo import fields, api, models, _
+import ast
 
 
 class Task(models.Model):
@@ -8,6 +9,13 @@ class Task(models.Model):
         """
         Action to open the task overview in kanban view
         """
-        action = self.project_id.with_context(active_id=self.project_id.id, active_ids=self.project_id.ids,
-                                              active_model=self.project_id._name).action_view_tasks()
+        action = self.env['ir.actions.act_window'].with_context({'active_id': self.project_id.id})._for_xml_id('project.act_project_project_2_project_task_all')
+        action['display_name'] = _("%(name)s", name=self.project_id.name)
+        context = action['context'].replace('active_id', str(self.project_id.id))
+        context = ast.literal_eval(context)
+        context.update({
+            'create': self.active,
+            'active_test': self.active
+            })
+        action['context'] = context
         return action
